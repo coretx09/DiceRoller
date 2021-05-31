5065ca875e83dd4d090465e2838ac691fa8e9ebd
coretx09 <ngampiosauvet@gmail.com>
2021-05-31 16:54:06 +0200

    textview

--- app/src/main/java/com/example/diiceroller/MainActivity.kt
+++ app/src/main/java/com/example/diiceroller/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.diiceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+           val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "ZEN BURGER"
+        }
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
