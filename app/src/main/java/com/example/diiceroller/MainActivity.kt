fe88427787a9d85ea79ef5894da14c648d21b19b
coretx09 <ngampiosauvet@gmail.com>
2021-05-31 17:47:46 +0200

    dice

--- app/src/main/java/com/example/diiceroller/MainActivity.kt
+++ app/src/main/java/com/example/diiceroller/MainActivity.kt
@@ -11,13 +11,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-           val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "ZEN BURGER"
-        }
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    // build dice
+    private fun rollDice() {
+        val dice = Dice(6) // instantiation
+        val diceRoll = dice.roll() // save in variable number rolled
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
     }
 }
 
+// class des dice
 class Dice(private val numSides: Int) {
     fun roll(): Int {
         return (1..numSides).random()
